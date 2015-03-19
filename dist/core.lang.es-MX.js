020af0ab1efab07ffaa3e27ceda541156be8378c
Hans <hans.php@gmail.com>
2015-03-19 15:43:34 -0600

    Inits...

---
+++ dist/core.lang.es-MX.js
@@ -0,0 +1,15 @@
+/*!
+ * Language-Manager-JS v1.0.0 (http://usermyadmin.net/Language-Manager-JS)
+ * Copyright 2015 EdificApps.
+ * Licensed under MIT (https://github.com/edificapps/Language-Manager-JS/blob/master/LICENSE)
+ *
+ * Manifest of language.
+ *
+ */
+$.language = {
+	LOGIN:{
+		signin: "Iniciar Sesión",
+		signin1: "Ingreso al sistema",
+		help: "Ayuda"
+	}
+}
